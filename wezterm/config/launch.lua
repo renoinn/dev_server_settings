b6265ce16f5f5553d5ab90a2efc7cb000d44ce57
renoinn <oomori.lunch@gmail.com>
2024-06-24 10:52:36 +0900

    fix default_prog for win

--- wezterm/config/launch.lua
+++ wezterm/config/launch.lua
@@ -6,12 +6,14 @@ local options = {
 }
 
 if platform.is_win then
-   options.default_prog = { 'pwsh' }
+   options.default_prog = {"/usr/bin/env", "zsh", "-l", "-c", "exec /usr/bin/env zsh"}
+   options.default_domain = 'WSL:Ubuntu'
    options.launch_menu = {
       { label = 'PowerShell Core', args = { 'pwsh' } },
       { label = 'PowerShell Desktop', args = { 'powershell' } },
       { label = 'Command Prompt', args = { 'cmd' } },
       { label = 'Nushell', args = { 'nu' } },
+      { label = 'Zsh', args = {"/usr/bin/env", "zsh", "-l", "-c", "exec /usr/bin/env zsh"} }
    }
 elseif platform.is_mac then
    options.default_prog = { 'zsh', '-l' }
